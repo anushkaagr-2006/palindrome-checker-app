fcc3445d7816af0cfe9cdc0b23a08487482b9b80
anushkaagr-2006 <agrawalanushkakumari944@gmal.com>
2026-03-16 15:15:33 +0530

    uc4

--- palindrome/src/PalindromCheckerApp.java
+++ palindrome/src/PalindromCheckerApp.java
@@ -1,18 +1,21 @@
-
-class UseCase2PalindromeCheckerApp {
+class UseCase4PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String text = "madam";
-        String reversed = "";
-        for (int i = text.length() - 1; i >= 0; i--) {
-            reversed = reversed + text.charAt(i);
+        String text = "radar";
+        char[] charArray = text.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if (text.equals(reversed)) {
-            System.out.println("Input text: " + text);
-            System.out.println("Is it a Palindrome? : true");
-        } else {
-            System.out.println("Input text: " + text);
-            System.out.println("Is it a Palindrome? : false");
-        }
+        System.out.println("Input text: " + text);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
